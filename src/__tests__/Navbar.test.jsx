f4b8ec0c7f03bd85f501517d1ee46695c46dcdff
Hassan Nawaz <hassanpu670@gmail.com>
2024-10-15 10:09:11 +0500

    "Removed commented out code in Navbar.test.jsx and Home.jsx files"

--- src/__tests__/Navbar.test.jsx
+++ src/__tests__/Navbar.test.jsx
@@ -1,84 +1,84 @@
-// import { fireEvent, render, screen, waitFor } from '@testing-library/react'
-// import { BrowserRouter } from 'react-router-dom';
-// import Navbar from '../components/navbar/Navbar'
-// import { Provider } from 'react-redux';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar'
+import { Provider } from 'react-redux';
 
 
 
-// test('test 1 ', () => {
-//     <Provider>
-//         render(<Navbar />)
-//     </Provider>
+test('test 1 ', () => {
+    <Provider>
+        render(<Navbar />)
+    </Provider>
 
-//     const head = screen.queryByLabelText('web-name');
+    const head = screen.queryByLabelText('web-name');
 
-//     if (head) {
-//         expect(head).toBeInTheDocument("Present in the document");
-//     }
-//     else {
-//         expect(head).not.toBeInTheDocument("Not present in the document");
-//     }
-// });
+    if (head) {
+        expect(head).toBeInTheDocument("Present in the document");
+    }
+    else {
+        expect(head).not.toBeInTheDocument("Not present in the document");
+    }
+});
 
-// test('test 2 ', () => {
-//     <Provider>
-//         render(<Navbar />)
-//     </Provider>
-//     const hedName = screen.queryByLabelText("store-name");
-//     if (hedName) {
-//         expect(hedName).toBeInTheDocument();
-//     }
-//     else {
-//         expect(hedName).not.toBeInTheDocument("Not present in the document");
-//     }
-// });
+test('test 2 ', () => {
+    <Provider>
+        render(<Navbar />)
+    </Provider>
+    const hedName = screen.queryByLabelText("store-name");
+    if (hedName) {
+        expect(hedName).toBeInTheDocument();
+    }
+    else {
+        expect(hedName).not.toBeInTheDocument("Not present in the document");
+    }
+});
 
-// test('test 3 ', () => {
-//     <Provider>
-//         render(<Navbar />)
-//     </Provider>
-//     const hedName = screen.queryByLabelText("product-page");
-//     if (hedName) {
-//         expect(hedName).toBeInTheDocument();
-//     }
-//     else {
-//         expect(hedName).not.toBeInTheDocument("Not present in the document");
-//     }
-// });
+test('test 3 ', () => {
+    <Provider>
+        render(<Navbar />)
+    </Provider>
+    const hedName = screen.queryByLabelText("product-page");
+    if (hedName) {
+        expect(hedName).toBeInTheDocument();
+    }
+    else {
+        expect(hedName).not.toBeInTheDocument("Not present in the document");
+    }
+});
 
-// test('render the all products ', () => {
-//     <Provider>
-//         render(
-//         <BrowserRouter>
-//             <Navbar />
-//         </BrowserRouter>
-//         );
-//     </Provider>
+test('render the all products ', () => {
+    <Provider>
+        render(
+        <BrowserRouter>
+            <Navbar />
+        </BrowserRouter>
+        );
+    </Provider>
 
 
-//     const linkElement = screen.queryByTestId('product-page');
-//     if (linkElement) {
-//         expect(linkElement).toBeInTheDocument();
-//     }
-//     else {
+    const linkElement = screen.queryByTestId('product-page');
+    if (linkElement) {
+        expect(linkElement).toBeInTheDocument();
+    }
+    else {
 
-//         expect(linkElement).not.toBeInTheDocument();
-//     }
-// });
+        expect(linkElement).not.toBeInTheDocument();
+    }
+});
 
-// test('expected router is clicked on linkk ', async () => {
-//     <Provider>
-//         render(
-//         <BrowserRouter>
-//             <Navbar />
-//         </BrowserRouter>
-//         );
-//     </Provider>
+test('expected router is clicked on linkk ', async () => {
+    <Provider>
+        render(
+        <BrowserRouter>
+            <Navbar />
+        </BrowserRouter>
+        );
+    </Provider>
 
-//     const linkElement = screen.queryByTestId('product-page');
-//     await waitFor(() => {
-//         expect(linkElement).toBeInTheDocument();
-//     });
-//     fireEvent.click(linkElement);
-//     expect(window.location.pathname).toBe('/');
-// })
+    const linkElement = screen.queryByTestId('product-page');
+    await waitFor(() => {
+        expect(linkElement).toBeInTheDocument();
+    });
+    fireEvent.click(linkElement);
+    expect(window.location.pathname).toBe('/');
+})
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,6 @@ import Layout from '../../components/layout/Layout'
 import HeroSection from '../../components/heroSection/HeroSection';
 import Filter from '../../components/filter/Filter';
 import ProductCard from '../../components/productcard/ProductCard';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addToCart, deleteFromCart } from '../../redux/cartSlice';
 
 function Home() {
   return (
